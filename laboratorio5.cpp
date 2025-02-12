4941534fbe110e7517c8fcd141d35993212e4113
gerson77-p <gersondavidperez76@gmail.com>
2025-02-11 18:23:06 -0600

    comentario nuevo

--- laboratorio5.cpp
+++ laboratorio5.cpp
@@ -1,6 +1,6 @@
 // laboratorio5.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
-
+/// nuevo comentario
 #include <iostream>
 using   namespace std;
 
